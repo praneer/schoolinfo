59da88761cd2897bfaa92f8381707335548c8d70
Pradhan, Neera <Neera.Pradhan@echostar.com>
2016-11-22 15:31:47 -0700

    Working

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
-var express = require("express")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var path = require("path");
 
+//controllers
+var schoolController = require("./controllers/schoolController");
+
+//Express request pipeline
 var app = express();
 app.use(express.static(path.join(__dirname, "../app/dist")));
-app.listen(7777, function(){
-    console.log("started listening on port ", 7777);
-})
+app.use(bodyParser.json())
+app.use("/api", schoolController);
+
+app.listen(7777, function () {
+    console.log("Started listening on port", 7777);
+});
+
+// Connect to mongodb database
+mongoose.connect("mongodb://localhost/schoolfinder");
